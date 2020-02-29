2c038ae02a5c5e39cde8a377bfc79cbf5924adb4
Thyago Volpatto <tvolpatto@gmail.com>
2020-02-28 19:59:59 -0700

    add JAWS connection property

--- config/connection.js
+++ config/connection.js
@@ -1,18 +1,25 @@
 const mysql = require("mysql");
 
-const connection = mysql.createConnection({
-    host: "localhost",
+var connection;
 
-    // Your port; if not 3306
-    port: 3306,
+if(process.env.JAWS_URL) {
+    connection = mysql.createConnection(process.env.JAWS_URL);
+} else {
+    connection = mysql.createConnection({
+       host: "localhost",
+   
+       // Your port; if not 3306
+       port: 3306,
+   
+       // Your username
+       user: "root",
+   
+       // Your password
+       password: process.env.MYSQL_PASSWORD,
+       database: "burgers_db"
+   });
 
-    // Your username
-    user: "root",
-
-    // Your password
-    password: process.env.MYSQL_PASSWORD,
-    database: "burgers_db"
-});
+}
 
 connection.connect(function (err) {
     if (err) throw err;
